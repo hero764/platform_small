bcd59f6e526c35c0c5350244c159da691bb1c58b
Sponser2973 <39594720+Sponser2973@users.noreply.github.com>
2018-05-30 16:49:29 +0300

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -1,27 +1,27 @@
-#include <Radio.h>
+#include <Radio.h>             //библиотека для радиопередачи
 #include <SPI.h>          
 #include "nRF24L01.h"    
 #include "RF24.h"        
 
 
-int msg[2];
+int msg[2];                    //вводим массив для передачи координат X и Y
 int x;
 int y;
-Lbr tr;
+Lbr tr;                       //обращаемся к структуре lbr в radio.h, в дальнейшем обращаться к ней будем через "tr"
 
 void setup() {
   Serial.begin(9600); 
-  tr.begin();
-  tr.startWriting(0);  
+  tr.begin();                
+  tr.startWriting(0);        //обращаемся к функции startWriting() в структуре lbr находящейся в библиотеке radio.cpp, в скобочках пишем номер канала по которому бедт идти предача
 }
 
 void loop() {
-   x = analogRead(A0);         
-   y = analogRead(A1);
+   x = analogRead(A0);       //считывание значений со входа А0
+   y = analogRead(A1);       //считывание значений со входа А1
    Serial.print("transmitted x: ");
      Serial.println(x);
      Serial.print("transmitted y: ");
      Serial.println(y);
-    tr.transmitte(x, y);    
+    tr.transmitte(x, y);    //обращаемся к функции trinsmitte в структуре lbr находящейся в библиотеке radio.cpp
   delay(10);
 }
